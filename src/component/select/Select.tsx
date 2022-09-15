4ad73137fd555ac53814349406539ce3d2297030
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-09-15 21:15:26 +0600

    basic select component: select + deselect, both on-click

--- src/component/select/Select.tsx
+++ src/component/select/Select.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './Select.module.scss';
 
-const Select = () => <div>select ping pong</div>;
+const ITEMS = [
+  {
+    key: 'a',
+    value: 'a',
+  },
+  {
+    key: 'b',
+    value: 'b',
+  },
+  {
+    key: 'c',
+    value: 'd',
+  },
+];
+
+const Select = () => {
+  const [opened, setOpened] = useState<boolean>(false);
+  const [options, setOptions] = useState(ITEMS);
+  const [selectedKeys, setSelectedKeys] = useState<string[]>([]);
+
+  const toggleOpened = () => setOpened(!opened);
+
+  const selectItem = (key: string) => setSelectedKeys([...selectedKeys, key]);
+  const removeSelectedItem = (key: string) => setSelectedKeys(selectedKeys.filter((selectedKey) => selectedKey !== key));
+
+  return (
+    <div>
+      <div>Selected Items</div>
+      {
+          options
+            .filter((option) => selectedKeys.includes(option.key))
+            .map((option) => (
+              <div
+                role="presentation"
+                onClick={() => { removeSelectedItem(option.key); }}
+              >
+                {option.value}
+              </div>
+            ))
+      }
+      <div onClick={() => { toggleOpened(); }} role="presentation">Menu</div>
+      {opened &&
+        <div className={styles.items}>
+            {
+                options
+                  .filter((option) => !selectedKeys.includes(option.key))
+                  .map((option) => (
+                    <div
+                      role="presentation"
+                      onClick={() => { selectItem(option.key); }}
+                    >
+                      {option.value}
+                    </div>
+                  ),
+                  )
+            }
+        </div>
+        }
+    </div>
+  );
+};
 
 export { Select };
