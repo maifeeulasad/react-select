10e832e1e2ceed3e21b428312bba840af1465f9f
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-09-15 19:02:56 +0600

    //empty react project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import { Landing } from './component/landing/Landing';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/react-select" element={<Landing />} />
+      <Route path="*" element={<Navigate to="/react-select" replace />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+// eslint-disable-next-line import/no-default-export
+export default App;
